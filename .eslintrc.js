461047944a011b90dae03eee638c5673e645e2f2
fatteme <fatt3me@gmail.com>
2025-06-19 19:26:27 -0700

    fix: azure deployment
    fix linting script

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,24 +3,27 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2022,
     sourceType: 'module',
+    project: './tsconfig.json',
   },
   plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
   ],
   rules: {
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/prefer-const': 'error',
+    '@typescript-eslint/no-var-requires': 'error',
+    '@typescript-eslint/no-empty-function': 'warn',
     'no-console': 'warn',
     'prefer-const': 'error',
     'no-var': 'error',
+    'no-unused-vars': 'off',
   },
   env: {
     node: true,
     es2022: true,
   },
+  ignorePatterns: ['dist/', 'node_modules/', '*.js'],
 }; 
